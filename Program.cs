6632e74fb1c65778f539024f37dfafa6e69ce4f0
M Malmsten <web@easyrider.nu>
2015-05-05 11:20:23 +0200

    Availability to check fields

--- Program.cs
+++ Program.cs
@@ -12,8 +12,16 @@ namespace Sudoku
         {
 
             Sudoku sudoku = new Sudoku();
-            sudoku.DrawTable();
-            sudoku.ChoosePosition();
+            
+            while (true)
+            {
+                sudoku.DrawTable();
+                sudoku.ChoosePosition();
+                sudoku.CheckPosition();
+                sudoku.CheckSquare();
+                sudoku.CheckLines();
+                sudoku.CheckIfValid();
+            }
 
         }
 
@@ -24,10 +32,11 @@ namespace Sudoku
         public int[,] table = new int[9, 9];
         public int userX = 0;
         public int userY = 0;
-        public bool empty;
-        public bool inSquare;
-        public bool inY;
-        public bool inX;
+        public int newNumber = 0;
+        public bool isValid = true;
+        public int inSquare;
+        public int minY;
+        public int minX;
 
         public void DrawTable()
         {
@@ -85,24 +94,127 @@ namespace Sudoku
             Console.WriteLine("Ange rad:");
             var input = Console.ReadLine();
             Int32.TryParse(input, out userY);
+            userY--;
 
             Console.WriteLine("Ange kolumn:");
             input = Console.ReadLine();
             Int32.TryParse(input, out userX);
+            userX--;
+
+            Console.WriteLine("Ange nummer:");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out newNumber);
 
         }
 
         public void CheckPosition()
         {
+            if (table[userY, userX] != 0)
+                isValid = false;
+        }
 
-            if (table[userY, userX] == 0)
-                empty = true;
+        public void CheckSquare()
+        {
+            if (userY >= 0 && userY < 3 && userX >= 0 && userX <= 2)
+                inSquare = 1;
+            else if (userY >= 0 && userY < 3 && userX > 2 && userX <= 5)
+                inSquare = 2;
+            else if (userY >= 0 && userY < 3 && userX > 5 && userX <= 8)
+                inSquare = 3;
+            else if (userY >= 3 && userY < 6 && userX >= 0 && userX <= 2)
+                inSquare = 4;
+            else if (userY >= 3 && userY < 6 && userX > 2 && userX <= 5)
+                inSquare = 5;
+            else if (userY >= 3 && userY < 6 && userX > 5 && userX <= 8)
+                inSquare = 6;
+            else if (userY >= 6 && userY < 9 && userX >= 0 && userX <= 2)
+                inSquare = 7;
+            else if (userY >= 6 && userY < 9 && userX > 2 && userX <= 5)
+                inSquare = 8;
+            else if (userY >= 6 && userY < 9 && userX > 5 && userX <= 8)
+                inSquare = 9;
+
+            switch (inSquare)
+            {
+                case 1:
+                    minX = 0;
+                    minY = 0;
+                    break;
+                case 2:
+                    minX = 3;
+                    minY = 0;
+                    break;
+                case 3:
+                    minX = 6;
+                    minY = 0;
+                    break;
+                case 4:
+                    minX = 0;
+                    minY = 3;
+                    break;
+                case 5:
+                    minX = 3;
+                    minY = 3;
+                    break;
+                case 6:
+                    minX = 6;
+                    minY = 3;
+                    break;
+                case 7:
+                    minX = 0;
+                    minY = 6;
+                    break;
+                case 8:
+                    minX = 3;
+                    minY = 6;
+                    break;
+                case 9:
+                    minX = 6;
+                    minY = 6;
+                    break;
+                default:
+                    minX = 0;
+                    minY = 0;
+                    Console.WriteLine("Error!!!");
+                    break;
+            }
+
+            for (int i = minY; i <= minY + 2; i++)
+            {
+                for (int j = minX; j <= minX + 2; j++)
+                {
+                    if (table[userY, userX] == newNumber)
+                        isValid = false;
+                }                
+            }
+        }
+        public void CheckLines()
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                if (table[userY, i] == newNumber)
+                    isValid = false;
+            }
+            for (int i = 0; i <= 8; i++)
+            {
+                if (table[i, userX] == newNumber)
+                    isValid = false;
+            }
+        }
+        public void CheckIfValid()
+        {
+            if (!isValid)
+            {
+                Console.WriteLine("Wrong! try again");
+            }
             else
-                empty = false;
-
-
-           
-
+            {
+                table[userY, userX] = newNumber;
+                Console.WriteLine("Correct!");
+            }
+            Console.WriteLine("Press any key to continue playing.");
+            Console.ReadKey();
+            Console.Clear();
         }
    
     }
